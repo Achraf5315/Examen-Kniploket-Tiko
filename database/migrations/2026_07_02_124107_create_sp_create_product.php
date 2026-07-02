a337500df467487d6f290c51989ff19e5a0927a9
wesley <340308@student.mboutrecht.nl>
2026-07-02 14:55:55 +0200

    stored procedure opgelost

--- database/migrations/2026_07_02_124107_create_sp_create_product.php
+++ database/migrations/2026_07_02_124107_create_sp_create_product.php
@@ -7,9 +7,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        DB::unprepared('DROP PROCEDURE IF EXISTS sp_CreateProduct');
-
-        $sql = file_get_contents(base_path('database/createscript/Storeproductrequest.sql'));
+        $sql = file_get_contents(base_path('database/createscript/stored_procedures/sp_create_product.sql'));
 
         if ($sql === false) {
             throw new \RuntimeException('SQL-bestand voor sp_CreateProduct kon niet worden geladen.');
